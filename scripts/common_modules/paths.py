a607a35c79c8195bef701c8b6847a39839e8e6a3
Gakuto1112 <gakuto131112@gmail.com>
2026-07-23 22:40:55 +0900

    入出力のパスにセッター関数を追加

--- scripts/common_modules/paths.py
+++ scripts/common_modules/paths.py
@@ -8,6 +8,16 @@ class Paths:
 	レポジトリのルートディレクトリのパス
 	"""
 
+	_input_locale_path: Path = _root / "src" / "japanese.tsv"
+	"""
+	入力データとなるロケールデータのパス
+	"""
+
+	_output_locale_path: Path = _root / "dist" / "japanese.tsv"
+	"""
+	ビルド済みのロケールデータの出力パス
+	"""
+
 	@property
 	def root(self) -> Path:
 		"""
@@ -22,7 +32,15 @@ class Paths:
 		入力データとなるローカルデータのパス
 		"""
 
-		return self._root / "src" / "japanese.tsv"
+		return self._input_locale_path
+
+	@input_locale_path.setter
+	def input_locale_path(self, path: Path) -> None:
+		"""
+		input_locale_pathのセッター関数
+		"""
+
+		self._input_locale_path = path
 
 	@property
 	def output_locale_path(self) -> Path:
@@ -30,6 +48,14 @@ class Paths:
 		ビルド済みのローカルデータの出力パス
 		"""
 
-		return self._root / "dist" / "japanese.tsv"
+		return self._output_locale_path
+
+	@output_locale_path.setter
+	def output_locale_path(self, path: Path) -> None:
+		"""
+		output_locale_pathのセッター関数
+		"""
+
+		self._output_locale_path = path
 
 paths = Paths()
