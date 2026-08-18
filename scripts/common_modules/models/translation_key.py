033f2e692330566b5fb305f5bc10953ef3bf3f93
Gakuto1112 <gakuto131112@gmail.com>
2026-08-18 21:24:26 +0900

    コメントの形式を修正

--- scripts/common_modules/models/translation_key.py
+++ scripts/common_modules/models/translation_key.py
@@ -3,16 +3,20 @@ from dataclasses import dataclass
 @dataclass(frozen=True, slots=True)
 class TranslationKey:
 	"""
+	翻訳キー1つのデータを表すクラス
+	"""
+
+	id: str | None = None
+	"""
 	翻訳ID
 	"""
-	id: str | None = None
 
+	en: str | None = None
 	"""
 	英語原文
 	"""
-	en: str | None = None
 
+	jp: str | None = None
 	"""
 	和訳
 	"""
-	jp: str | None = None
