1cce77089761cdeba37d64bb1276938ee6a9f9b6
Gakuto1112 <gakuto131112@gmail.com>
2026-09-25 11:51:19 +0900

    Steamニュースからゲームアップデートを抽出する処理を追加

---
+++ scripts/update_check/models/game_update_entry.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from typing import Self
+import re
+
+from common_modules.logger import Logger
+from ..models.steam_news_entry import SteamNewsEntry
+
+
+@dataclass(frozen=True, slots=True)
+class GameUpdateEntry:
+	"""
+	Steamニュースから抽出したゲームのアップデートエントリーのデータモデル。
+	"""
+
+	version: str
+	"""
+	ゲームバージョン。
+	"""
+
+	title: str
+	"""
+	アップデートのタイトル名。
+	"""
+
+	@classmethod
+	def from_steam_news_entry(cls, steam_news_entry: SteamNewsEntry) -> Self | None:
+		"""
+		ゲームのアップデートのエントリーのSteamNewsEntryからGameUpdateEntryを生成する。
+		入力されたSteamNewsEntryがゲームアップデートのものではない場合はNoneを返す。
+
+		Args:
+			steam_news_entry (SteamNewsEntry): Steamニュースのエントリー。
+
+		Returns:
+			Self | None: ゲームアップデートのエントリーであればGameUpdateEntryのインスタンス、そうでなければNone。
+		"""
+
+		version = re.match(r"[vV](\d+\.){2}\d+", steam_news_entry.title)
+		if not isinstance(version, re.Match):
+			return None
+
+		update_title_match = re.match(r"[vV](\d+\.){2}\d+\s(-\s)?([^!]+)", steam_news_entry.title)
+		if isinstance(update_title_match, re.Match):
+			update_title = update_title_match.group(3)
+		else:
+			Logger.print_warning("Failed to extract game update title from news title.")
+			update_title = ""
+
+		return cls(
+			version=version.group(0),
+			title=update_title
+		)
--- scripts/update_check/modules/news_fetcher.py
+++ scripts/update_check/modules/news_fetcher.py
@@ -1,5 +1,4 @@
 import argparse
-from datetime import datetime
 import json
 import urllib.request
 import errno
@@ -8,6 +7,7 @@ from urllib.error import HTTPError, URLError
 from common_modules.logger import Logger
 from ..models.steam_news import SteamNews
 from ..models.steam_news_entry import SteamNewsEntry
+from ..models.game_update_entry import GameUpdateEntry
 
 
 class NewsFetcher:
@@ -26,7 +26,7 @@ class NewsFetcher:
 	"""
 
 	@classmethod
-	def fetchNews(cls) -> SteamNews:
+	def fetch_news(cls) -> SteamNews:
 		"""
 		APIを利用してSteamからStormworksのニュースを取得する。
 
@@ -61,6 +61,26 @@ class NewsFetcher:
 
 		return [entry for entry in news_entries if entry.date > from_timestamp]
 
+	@staticmethod
+	def extract_game_update_news(news_entries: list[SteamNewsEntry]) -> list[GameUpdateEntry]:
+		"""
+		Steamニュースのエントリーのリストからゲームのアップデートに関するニュースを抽出し、GameUpdateEntryのリストとして返す。
+
+		Args:
+			news_entries (list[SteamNewsEntry]): Steamニュースのエントリーのリスト。
+
+		Returns:
+			list[GameUpdateEntry]: ゲームのアップデートに関するニュースのリスト。
+		"""
+
+		game_update_news = []
+		for entry in news_entries:
+			game_update_entry = GameUpdateEntry.from_steam_news_entry(entry)
+			if game_update_entry is not None:
+				game_update_news.append(game_update_entry)
+
+		return game_update_news
+
 	@classmethod
 	def _set_debug_args(cls) -> None:
 		"""
@@ -92,7 +112,7 @@ class NewsFetcher:
 		Logger.print_info("Fetching Steam news...")
 
 		try:
-			news = cls.fetchNews()
+			news = cls.fetch_news()
 		except HTTPError as e:
 			Logger.print_error(f"Failed to fetch Steam news: got error response ({e.code})")
 			exit(errno.ECONNABORTED)
@@ -115,10 +135,11 @@ class NewsFetcher:
 		Logger.print_info("Successfully fetched Steam news.")
 
 		new_arrival_news = cls.filter_new_arrival_news(news.appnews.newsitems, cls._debug_last_timestamp)
+		new_arrival_updates = cls.extract_game_update_news(new_arrival_news)
 
-		Logger.print_debug("New arrival news:")
-		for entry in new_arrival_news:
-			Logger.print_debug(f"- {entry.title} ({datetime.fromtimestamp(entry.date)})")
+		Logger.print_debug("New arrival updates:")
+		for entry in new_arrival_updates:
+			Logger.print_debug(f"- {entry.version}: {entry.title}")
 		Logger.print_spacer(1)
 
 if __name__ == "__main__":
