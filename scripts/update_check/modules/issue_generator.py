3023bbea326005e220c4d3a61365a34e86c14547
Gakuto1112 <gakuto131112@gmail.com>
2026-09-25 15:25:18 +0900

    Issue作成時のエラーの責任範囲を変更

--- scripts/update_check/modules/issue_generator.py
+++ scripts/update_check/modules/issue_generator.py
@@ -2,7 +2,7 @@ import errno
 import re
 import os
 
-from github import Auth, Github, GithubException
+from github import Auth, Github
 
 from common_modules.logger import Logger
 from common_modules.paths import paths
@@ -84,6 +84,9 @@ class IssueGenerator:
 		Args:
 			title (str): 投稿するIssueのタイトル
 			content (str): 投稿するIssueの本文
+		Raises:
+			EnvironmentError: GITHUB_TOKENまたはGITHUB_REPOSITORY環境変数が設定されていない場合
+			GithubException: GitHub APIの操作中にエラーが発生した場合
 		"""
 
 		token = os.getenv("GITHUB_TOKEN")
@@ -92,21 +95,16 @@ class IssueGenerator:
 		if not token or not repository:
 			raise EnvironmentError("GITHUB_TOKEN or GITHUB_REPOSITORY environment variable is not set.")
 
-		try:
-			github = Github(auth=Auth.Token(token))
-			repository = github.get_repo(repository)
+		github = Github(auth=Auth.Token(token))
+		repository = github.get_repo(repository)
 
-			issue = repository.create_issue(
-				title=title,
-				body=content,
-				labels=["game update"],
-			)
+		issue = repository.create_issue(
+			title=title,
+			body=content,
+			labels=["game update"],
+		)
 
-			Logger.print_info(f"Issue created: #{issue.number} - #{title}")
-		except GithubException as e:
-			Logger.print_error(f"Failed to create issue: {str(e)}")
-			Logger.print_debug(f"{e.message}")
-			exit(errno.EIO)
+		Logger.print_info(f"Issue created: #{issue.number} - #{title}")
 
 	@classmethod
 	def debug(cls) -> None:
